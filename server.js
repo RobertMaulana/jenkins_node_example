51cab8cab705d4951734fc9355e8671ec4218e7a
Robert <robert@crowde.co>
2019-11-24 01:49:07 +0700

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Welcome to the jungle\n');
+  res.send('Welcome to the jungle 2\n');
 });
 
 app.listen(PORT, HOST);
