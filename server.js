e3ef22767985b805c755429a2e8b82a22c701712
Robert <robert@crowde.co>
2019-11-26 00:57:13 +0700

    update

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('delete local docker images\n');
+  res.send('SET CONDITIONS\n');
 });
 
 app.listen(PORT, HOST);
